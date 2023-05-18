ca04af15e73f57914dbdafc6c69ce09f5e18c856
Kirems <it@casinodays.com>
2023-05-18 14:28:26 +0200

    removed config map

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Learn REact
+         Learn {process.env.REACT_APP_KUKU}
         </a>
       </header>
     </div>
